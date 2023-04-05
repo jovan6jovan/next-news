86edf538564858e413c0af354f5958ee268f6706
jovan6jovan <jovan6jovanovic@gmail.com>
2023-04-05 18:18:05 +0200

    basic setup, next + bulma css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { FC } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App: FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <div>
+      <Head>
+        <title>Next News</title>
+        <meta
+          name="description"
+          content="A news website created with Next.js"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        ></meta>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
+};
+
+export default App;
---
+++ pages/search.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const SearchNewsPage: FC = () => {
+  return <div>SearchNewsPage</div>;
+};
+
+export default SearchNewsPage;
